a805983e5777484e5efbdd56c1ff3b9849e0af1f
Dan  Pereda <dpereda@gmail.com>
2018-03-01 18:16:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyRemoveFirstKitten(name){
 }
 
 function appendKitten(name){
-  var newKittens = new array (kittens)
+  var newKittens = kittens.slice()
   
   newKittens.push(name)
   
