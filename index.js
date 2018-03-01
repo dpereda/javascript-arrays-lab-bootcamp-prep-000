44b96b685b69c5873d17856ab1057fddd39b70a8
Dan  Pereda <dpereda@gmail.com>
2018-03-01 18:21:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,10 @@ function prependKitten(name){
   newKittens.unshift(name)
   return newKittens
 }
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
 
 
