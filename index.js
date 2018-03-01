05988d7f09af0960f426d8bf7e2c919f4d42628a
Dan  Pereda <dpereda@gmail.com>
2018-03-01 18:18:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,4 +23,10 @@ function appendKitten(name){
   newKittens.push(name)
   return newKittens
 }
+function prependKitten(name){
+  var newKittens = (...kittens)
+  newKittens.shift(name)
+  return newKittens
+}
+
 
