582e08a896a05300662dceb759b2e08782dd4386
Dan  Pereda <dpereda@gmail.com>
2018-03-01 18:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  //console.log(kittens)
+  return kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  //console.log(kittens)
+  return kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name){
+  //console.log(kittens)
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(name){
+  //console.log(kittens)
+  kittens.shift()
+  return kittens
+}
+function appendKitten(name){
+  console.log(kittens)
+  
+  var newKittens = [kittens]
+  console.log(newKittens)
+  newKittens.push(name)
+  console.log(newKittens)
+  return
+}
+
+appendKitten("Joe")
